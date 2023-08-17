ce11196af0dbedd8e58628816fe28883980c928b
David Menache <dmenache@devopsandplatforms.com>
2023-08-17 05:04:24 -0600

    commiting changes including persistent undo!

--- init.lua
+++ init.lua
@@ -1,3 +1,13 @@
 require("dmenache")
 print("hello")
+if vim.fn.has('persistent_undo') == 1 then
+    local target_path = vim.fn.expand('~/.config/vim-persisted-undo/')
+
+    if vim.fn.isdirectory(target_path) ~= 1 then
+        vim.fn.system('mkdir -p ' .. target_path)
+    end
+
+    vim.o.undodir = target_path
+    vim.cmd('set undofile')
+end
 
